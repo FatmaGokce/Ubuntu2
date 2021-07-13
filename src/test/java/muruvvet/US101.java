a56ad196537cd59724e49a9fc6a21ef6b1238174
MDogan <mdoganusa@gmail.com>
2021-07-12 21:24:23 -0400

    more stuff added

--- src/test/java/muruvvet/US101.java
+++ src/test/java/muruvvet/US101.java
@@ -4,5 +4,8 @@ public class US101 {
     public static void main(String[] args) {
         System.out.println("hello everyone");
         //us101 started
+        for (int i = 0; i < 9; i++) {
+            System.out.println("*");
+        }
     }
 }
