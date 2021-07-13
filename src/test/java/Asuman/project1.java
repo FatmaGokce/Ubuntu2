98add4a13d41d64d2aec5e2368e58bef61ff9eba
asuman <ozarasuman@gmail.com>
2021-07-12 21:17:57 -0400

    second commit - Asuman

--- src/test/java/Asuman/project1.java
+++ src/test/java/Asuman/project1.java
@@ -3,5 +3,6 @@ package Asuman;
 public class project1 {
     public static void main(String[] args) {
         System.out.println("Lets start");
+        System.out.println("Second change");
     }
 }
