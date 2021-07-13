67335089f713bd33910b1e06cec248d3ee3e8e07
yildiz ucar <yildizucr@gmail.com>
2021-07-12 21:22:38 -0400

    first commit 2

--- src/test/java/Yildiz/Ucar.java
+++ src/test/java/Yildiz/Ucar.java
@@ -3,6 +3,9 @@ package Yildiz;
 public class Ucar {
     public static void main(String[] args) {
         System.out.println("  lets do it guys !!!!");
+
+        System.out.println(" yes wi did ");
+
     }
 
 }
